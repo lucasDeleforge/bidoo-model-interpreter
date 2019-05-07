087d37f12bc0f942693deefa1eb7329fe3c1bcbf
bidoo <bidoo@bidoos-MacBook-Air.local>
2019-05-08 00:52:18 +0200

    chore: pep8 typos

--- bdooModelInterpreter/linear/linearInterpreter.py
+++ bdooModelInterpreter/linear/linearInterpreter.py
@@ -5,7 +5,7 @@ from ..interpreter import Interpreter
 
 
 class LinearInterpreter(Interpreter):
-    def __init__(self, y_train, x_test,*args):
+    def __init__(self, y_train, x_test, *args):
         super().__init__(*args)
         self.y_train = y_train
         self.y_test = x_test
