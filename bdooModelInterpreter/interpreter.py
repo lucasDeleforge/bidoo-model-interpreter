cb62bff7da5fa223cf6a66fe957ef06c95cb4bd9
bidoo <bidoo@bidoos-MacBook-Air.local>
2019-05-07 22:45:27 +0200

    feat: Parent class Interpreter created with basic signatures

---
+++ bdooModelInterpreter/interpreter.py
@@ -0,0 +1,40 @@
+"""
+Main interpreter
+"""
+
+
+class Interpreter:
+    """
+    Parent class for all interpreter.
+    """
+    def __init__(self, model, train, test, validation=None):
+        self.model = model
+        self.train = train
+        self.test = test
+        self.validation = validation
+
+    def plot_feature_importance(self):
+        """
+
+        :return:
+        """
+
+        return None
+
+    def plot_data(self):
+        """
+
+        :return:
+        """
+
+        return None
+
+    def plot_performance(self):
+        """
+
+        :return:
+        """
+
+        return None
+
+
