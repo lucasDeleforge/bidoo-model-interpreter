91b192f2a4c127475ff6fb9eb7112cceb7119359
bidoo <bidoo@bidoos-MacBook-Air.local>
2019-05-08 00:51:43 +0200

    feat: y_train/test added to __init__

--- bdooModelInterpreter/linear/linearInterpreter.py
+++ bdooModelInterpreter/linear/linearInterpreter.py
@@ -3,6 +3,11 @@ Interpreter for GLM
 """
 from ..interpreter import Interpreter
 
+
 class LinearInterpreter(Interpreter):
-    def __init__(self,*args):
+    def __init__(self, y_train, x_test,*args):
         super().__init__(*args)
+        self.y_train = y_train
+        self.y_test = x_test
+
+
