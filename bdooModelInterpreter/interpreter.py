4bdd284e3f8e3be561e87a20441b6af460ba1f71
bidoo <bidoo@bidoos-MacBook-Air.local>
2019-05-08 02:17:29 +0200

    feat: plot of feature importance for LinearModel

--- bdooModelInterpreter/interpreter.py
+++ bdooModelInterpreter/interpreter.py
@@ -2,7 +2,12 @@
 Main interpreter
 """
 import logging
+import pandas as pd
+import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set(style="white", context="talk")
+
 
 class Interpreter:
     """
@@ -14,12 +19,28 @@ class Interpreter:
         self.X_train = X_train
         self.X_test = X_test
         self.validation = validation
+        self.features = self.X_train.columns.values
 
-    def plot_feature_importance(self):
+    def plot_feature_importance(self, nb_features=20):
+        """
+        Plot importances of self.features, according to self.model.coef_
+        :param nb_features: Number of most important features to keep.
+        :return: matplotlib.pyplot.plt figures
         """
 
-        :return:
-        """
+        df_plot = pd.DataFrame(np.column_stack((self.features, self.model.coef_)), columns=['feature', 'coef'],
+                               index=None)
+        df_plot['coef_abs'] = df_plot.coef.abs()
+        df_plot = df_plot.sort_values(by=['coef_abs'], ascending=False).head(nb_features)
+        df_plot.sort_values(by=['coef'], inplace=True)
+
+        fig = self.init_plt_fig(1,1)
+        sns.barplot(data=df_plot, x='feature', y='coef', palette="rocket", ax=fig[1])
+        fig[1].axhline(0, color="k", clip_on=False)
+        fig[1].set_ylabel("Sequential")
+
+        plt.tight_layout(h_pad=2)
+        plt.plot()
 
         return None
 
@@ -46,5 +67,16 @@ class Interpreter:
         """
 
         return None
+
+    def init_plt_fig(self, rows, cols):
+        """
+        Standardization of figure creation.
+        :param rows: Number of axis in rows
+        :param cols: Number of axis in columns
+        :return: matplotlib.pyplot.plt figures with rows*columns shape
+        """
+        fig = plt.subplots(rows, cols, figsize=(7, 5), sharex=True)
+
+        return fig
 
 
